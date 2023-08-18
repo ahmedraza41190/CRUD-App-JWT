b3df2b9143bb7b1dd3c276ccb5fd24f19c8cb449
Ahmed Raza <ragix.aemi@gmail.com>
2023-08-18 14:24:17 +0500

    first commit

--- server.mjs
+++ server.mjs
@@ -1,21 +1,53 @@
-import express from 'express'
+import express from 'express';
 import path from 'path';
+import jwt from 'jsonwebtoken';
+import 'dotenv/config'
 const __dirname = path.resolve();
 
-import apiv1Router from './apiv1/main.mjs'
-// import apiv2Router from './apiv2/main.mjs'
+import authRouter from './routes/auth.mjs'
+import postRouter from './routes/post.mjs'
+import cookieParser from 'cookie-parser'
+import { decode } from 'punycode';
 
-const app = express()
-
+const app = express();
 app.use(express.json()); // body parser
+app.use(cookieParser()); // cookie parser
+app.use('/login', express.static(path.join(__dirname, 'public/login')))
+app.use('/signup', express.static(path.join(__dirname, 'public/signup')))
 
-app.use("/api/v1", apiv1Router)
-// app.use("/api/v2", apiv2Router)
+app.use("/api/v1", authRouter)
 
+
+app.use((req, res, next) => {
+    console.log("cookies: ", req.cookies);
+
+    const token = req.cookies.token;
+
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET);
+        console.log("decoded: ", decoded);
+
+        req.body.decoded = {
+            firstName: decoded.firstName,
+            lastName: decoded.lastName,
+            email: decoded.email,
+            isAdmin: decoded.isAdmin,
+        };
+
+        next();
+
+    } catch (err) {
+        res.status(401).send(`
+           Invalid
+            `)
+    }
+
+})
+
+app.use("/api/v1", postRouter)
 app.use(express.static(path.join(__dirname, 'public')))
 
-const PORT = process.env.PORT || 3000
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`)
+    console.log(`Example server listening on port ${PORT}`)
 })
